ee8a402306a4cedde4d3794a50164ab8ff9b77ae
Brooke Kullberg and Reese Lee <reese@>
2019-03-26 16:14:28 -0700

    add definitions that fade in after clicking the title of the flashcard

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-  $()
-
-
+  $(".title").click(function() {
+    $(".definition").hide();
+    var defid = event.target.id;
+    var define = "#" + defid.replace("title", "def");
+    $(define).fadeIn("slow");
+  })
 })
